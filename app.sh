3a1cd726686898a977a53ba01f54c2c9ffb7e5ff
dhomas1 <dom@cotugno.co>
2025-08-12 07:29:34 -0400

    Update app.sh
    Upgrade SyncThing to v2.0.0 from source

--- app.sh
+++ app.sh
@@ -1,7 +1,7 @@
 
 ### syncthing ###
 _build_syncthing() {
-  local VERSION="1.29.6"
+  local VERSION="2.0.0"
   local FOLDER="syncthing-linux-arm-v${VERSION}"
   local FILE="${FOLDER}.tar.gz"
   local URL="https://github.com/syncthing/syncthing/releases/download/v${VERSION}/${FILE}"
